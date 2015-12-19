0a320256e8501f04ab0b15b63501bf2b53928a14
henrychen917 <henrychen917@gmail.com>
2015-12-19 11:48:35 +0800

    update

---
+++ main.js
@@ -0,0 +1,20 @@
+var canvas = document.getElementById("game-canvas")
+var ctx = canvas.getContext("2d");
+
+var bgImg = document.createElement("img");
+bgImg.src = "images/gamemap.png";
+var heroImg = docment.createElement("img");
+heroImg.src = "images/jason.gif";
+
+var hero = {
+	x: 0,
+	y: 0
+}
+
+function draw(){
+ctx.drawImage(bgImg,0,0);
+	ctx.drawImage(heroImg, hero.x, hero.y);
+}
+
+
+setInterval(draw, 16)
