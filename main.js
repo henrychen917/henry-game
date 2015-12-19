09204233cc6a29008ae11e230405749d8d5a4dd6
henrychen917 <henrychen917@gmail.com>
2015-12-19 21:42:36 +0800

    Update main.js

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ function draw(){
 	ctx.drawImage(bgImg,0,0);
 	ctx.drawImage(heroImg, hero.x, hero.y);
 	ctx.drawImage(towerImg,0,0,64,64);
-	ctx.drawImage(prinsessImg,512,336,64,64);
+	ctx.drawImage(prinsessImg,512,64,64,64);
 }
 var cursor = {x:0, y:0};
 $("#game-canves").click(function(){
