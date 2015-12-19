85be1df8f035343a5b92eeb291b661f3dc4c1fcc
henrychen917 <henrychen917@gmail.com>
2015-12-19 21:15:24 +0800

    Update main.js

--- main.js
+++ main.js
@@ -7,8 +7,7 @@ var heroImg = document.createElement("img");
 heroImg.src = "images/jason.gif";
 var towerImg = document.createElement("img");
 towerImg.src = "images/tower-btn.png";
-var redImg = document.createElement("img");
-towerImg.src = "images/daigh.png";
+
 var hero = {
 	x: 0,
 	y: 0
@@ -18,7 +17,7 @@ function draw(){
 	ctx.drawImage(bgImg,0,0);
 	ctx.drawImage(heroImg, hero.x, hero.y);
 	ctx.drawImage(towerImg,0,0,64,64);
-	ctx.drawImage(redImg,400,0);
+	
 }
 var cursor = {x:0, y:0};
 $("#game-canves").click(function(){
