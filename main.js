b8f1d2b8989a3eb194165c12865dc408267cb896
henrychen917 <henrychen917@gmail.com>
2016-01-09 11:42:50 +0800

    Update main.js

--- main.js
+++ main.js
@@ -6,17 +6,45 @@ var cursor = {};
 var isBuilding = false;
 var tower = {};
 var enemy = { 
-    x:96-64, 
+    x:96, 
     y:480-32,
     direction:{x:0,y:-1},
     speed:64,
+    pathDes: 0,
     move: function(){
-        // this.x += this.direction.x * this.speed/FPS;
-        this.x = this.x + this.direction.x * this.speed/FPS;
-        // this.y += this.direction.y * this.speed/FPS;
-        this.y = this.y + this.direction.y * this.speed/FPS;
+        if( isCollided(enemyPath[this.pathDes].x, enemyPath[this.pathDes].y, this.x, this.y, this.speed/FPS, this.speed/FPS) ){
+
+            // 首先，移動到下一個路徑點
+            this.x = enemyPath[this.pathDes].x;
+            this.y = enemyPath[this.pathDes].y;
+
+            // 指定下一個路徑點
+            this.pathDes++;
+
+            // 取得前往下一個路徑點的單位向量
+            var unitVector = getUnitVector( this.x, this.y, enemyPath[this.pathDes].x, enemyPath[this.pathDes].y );
+            this.direction.x = unitVector.x;
+            this.direction.y = unitVector.y;
+
+        } else {
+            // this.x += this.direction.x * this.speed/FPS;
+            this.x = this.x + this.direction.x * this.speed/FPS;
+            // this.y += this.direction.y * this.speed/FPS;
+            this.y = this.y + this.direction.y * this.speed/FPS;
+        }
     }
 };
+
+var enemyPath = [
+    {x:96, y:64},
+    {x:384, y:64},
+    {x:384, y:192},
+    {x:224, y:192},
+    {x:224, y:320},
+    {x:544, y:320},
+    {x:544, y:96}
+];
+
 
 // ====== 引入圖檔 ====== //
 var bgImg = document.createElement("img");
@@ -30,6 +58,7 @@ slimeImg.src = "images/Decoration_goldenturd_thumbnail@2x.png";
 var panImg = document.createElement("img");
 panImg.src = "images/c_frying_pan_gold_large.1198945aaa4f8544f7f372d4dfb15228f0b6f9b5.png";
 // ==================== //
+
 
 $("#game-canvas").mousemove(function(event) {
     cursor = {
@@ -57,10 +86,10 @@ function draw(){
 
     ctx.drawImage(bgImg,0,0);
     ctx.drawImage(buttonImg, 640-64, 480-64, 64, 64);
-    ctx.drawImage(towerImg, tower.x, tower.y, 32,32);
-    ctx.drawImage(slimeImg, enemy.x, enemy.y , 32 ,32);
+    ctx.drawImage(towerImg, tower.x, tower.y);
+    ctx.drawImage(slimeImg, enemy.x, enemy.y);
     if(isBuilding){
-        ctx.drawImage(towerImg, cursor.x, cursor.y , 32 ,32);
+        ctx.drawImage(towerImg, cursor.x, cursor.y);
     }
 }
 
@@ -80,4 +109,16 @@ function isCollided(pointX, pointY, targetX, targetY, targetWidth, targetHeight)
     } else {
         return false;
     }
+}
+
+function getUnitVector(srcX, srcY, targetX, targetY) {
+    var offsetX = targetX - srcX;
+    var offsetY = targetY - srcY;
+    var distance = Math.sqrt( Math.pow(offsetX,2) + Math.pow(offsetY,2) );
+
+    var unitVector = {
+        x: offsetX/distance,
+        y: offsetY/distance
+    };
+    return unitVector;
 }
