f02c6e6515cffb0cec354c567a4ae0c8050778ee
henrychen917 <henrychen917@gmail.com>
2016-01-16 12:22:09 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,44 +1,43 @@
 var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");
-var clock = 0;
+
 var FPS = 60;
+var clock = 0;
 var cursor = {};
 var isBuilding = false;
 var tower = {};
 var enemies = [];
 
-function Enemy() {
-this.x = 96; 
-this.y = 480-32;
-this.direction = {x:0,y:-1};
-this.speed = 64;
-this.pathDes = 0;
-this.move =  function(){
+function Enemy() { 
+    this.x = 96; 
+    this.y = 480-32;
+    this.direction = {x:0,y:-1};
+    this.speed = 64;
+    this.pathDes = 0;
+    this.move = function(){
         if( isCollided(enemyPath[this.pathDes].x, enemyPath[this.pathDes].y, this.x, this.y, this.speed/FPS, this.speed/FPS) ){
-}
 
-    
-
-       
+            // 首先，移動到下一個路徑點
             this.x = enemyPath[this.pathDes].x;
             this.y = enemyPath[this.pathDes].y;
 
-   
+            // 指定下一個路徑點
             this.pathDes++;
 
-           
+            // 取得前往下一個路徑點的單位向量
             var unitVector = getUnitVector( this.x, this.y, enemyPath[this.pathDes].x, enemyPath[this.pathDes].y );
             this.direction.x = unitVector.x;
             this.direction.y = unitVector.y;
 
         } else {
-        
+            // this.x += this.direction.x * this.speed/FPS;
             this.x = this.x + this.direction.x * this.speed/FPS;
-       
+            // this.y += this.direction.y * this.speed/FPS;
             this.y = this.y + this.direction.y * this.speed/FPS;
         }
-    }
-};
+    };
+}
+
 
 var enemyPath = [
     {x:96, y:64},
@@ -50,6 +49,7 @@ var enemyPath = [
     {x:544, y:96}
 ];
 
+
 var bgImg = document.createElement("img");
 bgImg.src = "images/mapaaa.png";
 var buttonImg = document.createElement("img");
@@ -60,7 +60,6 @@ var slimeImg = document.createElement("img");
 slimeImg.src = "images/Decoration_goldenturd_thumbnail@2x.png";
 var panImg = document.createElement("img");
 panImg.src = "images/c_frying_pan_gold_large.1198945aaa4f8544f7f372d4dfb15228f0b6f9b5.png";
-
 
 
 $("#game-canvas").mousemove(function(event) {
@@ -84,29 +83,31 @@ $("#game-canvas").click(function(){
 });
 
 function draw(){
-    
-    if ((clock%80)==0){
-        var newEnemy=new Enemy();
-        enemies.push(newEnemy);
-        
-        
+
+    if(clock%80==0){
+        enemies.push(new Enemy());
     }
-    enemy.move();
 
     ctx.drawImage(bgImg,0,0);
     ctx.drawImage(buttonImg, 640-64, 480-64, 64, 64);
     ctx.drawImage(towerImg, tower.x, tower.y , 32 , 32);
-    ctx.drawImage(slimeImg, enemy.x, enemy.y , 32 , 32);
     if(isBuilding){
-        ctx.drawImage(towerImg, cursor.x, cursor.y , 32, 32);
+        ctx.drawImage(towerImg, cursor.x, cursor.y , 32 , 32);
     }
+
+    for(var i=0; i<enemies.length; i++){
+        enemies[i].move();
+        ctx.drawImage( slimeImg, enemies[i].x, enemies[i].y , 32 , 32);
+    }
+
+    clock++;
 }
 
 setInterval(draw, 1000/FPS);
 
 
 
-
+// ====== 其他函式 ====== //
 
 function isCollided(pointX, pointY, targetX, targetY, targetWidth, targetHeight) {
     if(     pointX >= targetX
